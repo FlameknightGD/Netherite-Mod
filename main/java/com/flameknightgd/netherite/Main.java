b92a31a57575f4ce431287abc669ef58176f3a23
FlameknightGD <61699155+FlameknightGD@users.noreply.github.com>
2021-04-09 20:01:45 +0200

    Netherite-1.0.0
    Updated Branch

---
+++ main/java/com/flameknightgd/netherite/Main.java
@@ -0,0 +1,42 @@
+package com.flameknightgd.netherite;
+
+import com.flameknightgd.netherite.init.ModRecipes;
+import com.flameknightgd.netherite.proxy.CommonProxy;
+import com.flameknightgd.netherite.util.Reference;
+import com.flameknightgd.netherite.util.handlers.RegistryHandler;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main 
+{
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event)
+	{
+		RegistryHandler.preInitRegistries();
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event)
+	{
+		ModRecipes.init();
+	}
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
